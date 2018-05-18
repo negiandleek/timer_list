fb3506aa5cb645cd316e3845f49274f0bde42bd1
youya66 <youya66.official@gmail.com>
2018-05-18 16:09:45 +0900

    add function to mange timer
    timers component and timer component relation to parent-children.

--- public/src/index.js
+++ public/src/index.js
@@ -1,32 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Timer extends React.Component{
+class TimerManager extends React.Component{
     constructor(props){
         super(props);
-        this.interval;
         this.state = {
-            count: 0
+            items: [[
+                {parent_id: 0, child_id: 0, count: 0}
+            ]]
         };
     }
-    componentDidMount(){
-        this.interval = setInterval(this.tick.bind(this), 1000)
-    }
     render(){
-        return (
-            <div className="timer">
-                {this.state.count}
+        return(
+            <div className="timer_list">
+                {this.state.items.map((items)=>(
+                    <Timers
+                        tick={this.tick.bind(this)}
+                        items={items}
+                    />
+                ))}
             </div>
         )
     }
     tick(){
-        this.setState((prevState)=>({
-            count: prevState.count + 1
+        const args = arguments[0];
+       
+        let new_state = this.state.items.slice();
+        new_state[args.parent_id][args.child_id].count += 1;
+      
+        this.setState(()=>({
+            items: new_state
         }));
     }
 }
 
+const Timers = (props) => {
+    return (
+        <ul>
+            {props.items.map((item, i) => (
+                <li>
+                    <Timer 
+                        item={item}
+                        tick={props.tick}
+                    />
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+class Timer extends React.Component{
+    constructor(props){
+        super(props);
+        this.interval;
+    }
+    render(){
+        return (
+            <div className="timer">
+                {this.props.item.count}
+                <div className="timer__btns">
+                    <input type="button" value="start" onClick={this.start.bind(this)} />
+                    <input type="button" value="stop" onClick={this.stop.bind(this)} />
+                </div>
+            </div>
+        )
+    }
+    start(){
+        const props = this.props.item;
+        this.interval = setInterval(() => this.props.tick(
+            {
+                parent_id: props.parent_id,
+                child_id: props.child_id
+            }
+        ), 1000);
+    }
+    stop(){
+        clearInterval(this.interval);
+    }
+}
+
 ReactDOM.render(
-    <Timer />,
+    <TimerManager />,
     document.getElementById("root")
 )
