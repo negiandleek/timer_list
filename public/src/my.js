f346e15db8fb6f8fd23a806949f3fd3191a65d87
youya <youya66@users.noreply.github.com>
2018-05-20 11:58:38 +0900

    add self module

--- public/src/my.js
+++ public/src/my.js
@@ -1,6 +1,20 @@
 (function (){
     const root = (typeof window == 'object' && window.self === self && window) ||
-                (typeof global == 'object' && global.global === global && global);
+                (typeof global == 'object' && global.global === global && global) ||
+                {};
 
     const previous = root.MY;
+
+    MY = function(){};
+
+    if (typeof exports !== 'undefined') {
+        // Support Node.js specific `module.exports`
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = MY;
+        }
+        // But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
+        exports.MY = MY;
+    }else{
+        root.MY = MY;
+    }
 })()
