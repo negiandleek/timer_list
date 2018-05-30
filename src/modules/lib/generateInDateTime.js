10d64b96eb55da59961c855e76ce8e80a4a0aa87
youya66 <youya66_official@gmail.com>
2018-05-30 17:07:47 +0900

    deal with alarm and count down in generate_in_date_time function

--- src/modules/lib/generateInDateTime.js
+++ src/modules/lib/generateInDateTime.js
@@ -2,6 +2,8 @@ import alpha from "./alpha";
 import is_time from "./isTime";
 import is_tommorow from "./isTommorow";
 import convert_alarm_to_time_left from "./convertAlarmToTimeLeft";
+import convert_str_to_time from "./convertStrToTime";
+import convert_time_to_milli from "./convertTimeToMilli";
 
 let order = alpha.time_order.slice();
 order = order.map((str)=>{
@@ -10,25 +12,25 @@ order = order.map((str)=>{
 order[3]+="econds";
 
 export default function generate_in_date_time(value, alarm=true){
-    if(alarm){
-        is_time(value);
-    }else{
-        
-    }
     let in_date_time = new Date();
-    let tommorow_flag = is_tommorow(value);
-    let target_date = value.split(alpha.time_split).map(Number);
-    let padding = new Array(order.length - target_date.length).fill(0)
-    target_date = target_date.concat(padding);
 
-    if(tommorow_flag){
-        in_date_time = new Date(in_date_time.setDate(in_date_time.getDate() + 1));
+    if(!alarm){
+        is_time(value);
+        let n = convert_str_to_time(value);
+        let m = convert_time_to_milli(n);
+        in_date_time = new Date(new Date().getTime() + m);
+    }else{
+        let tommorow_flag = is_tommorow(value);
+        let target_date = value.split(alpha.time_split).map(Number);
+        let padding = new Array(order.length - target_date.length).fill(0)
+        target_date = target_date.concat(padding);
+
+        if(tommorow_flag){
+            in_date_time = new Date(in_date_time.setDate(in_date_time.getDate() + 1));
+        }
+        order.forEach((name,i)=>{
+            in_date_time["set" + name](target_date[i])
+        })
     }
-    order.forEach((name,i)=>{
-        in_date_time["set" + name](target_date[i])
-    })
-
     return in_date_time;
 }
-
-generate_in_date_time("1650")
