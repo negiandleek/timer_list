02273aa24db058d1680b3725882d8cd87982bfb7
youya66 <youya66_official@gmail.com>
2018-06-05 17:57:18 +0900

    refactoring convertStrToMilli.js

--- src/modules/lib/alpha.js
+++ src/modules/lib/alpha.js
@@ -1,5 +1,6 @@
 const alpha = {
     time_split: /[0-9]{0}(?=(?:[0-9]{2})+$)/,
-    time_order: ["hours", "minutes", "seconds"]//[, "millis"]
+    time_order: ["hours", "minutes", "seconds"],//[, "millis"]
+    timer_based: [60 * 60 * 1000, 60 * 1000, 1000]
 }
 export default alpha;
--- src/modules/lib/convertStrToMilli.js
+++ src/modules/lib/convertStrToMilli.js
@@ -1,22 +1,23 @@
+import _ from "underscore";
+import alpha from "./alpha";
 import is_time_of_string from "./isTimeOfString";
 import convert_milli_time from "./convertMilliToTime";
 
+function convert(time, clock, startIndex = -2, count = 0){
+    let temp = time;
+    if(clock.length < -1 * startIndex) return temp;
+    temp[temp.length - (1 + count)] = clock.substr(startIndex, 2);
+    return convert(temp, clock, startIndex - 2, count + 1);
+}
+
 export default function convert_str_to_milli(value){
     is_time_of_string(value)
-
     // hour,minute,second;
     let time = [0,0,0];
-    let based = [60 * 60 * 1000, 60 * 1000, 1000];
-    let result = 0;
-
-    function convert(time, startIndex = -2, count = 0){
-        if(value.length < -1 * startIndex)return;
-        time[time.length - (1 + count)] = value.substr(startIndex, 2);
-        convert(time, startIndex - 2, count + 1);
-    }
-    convert(time);
-
-    time.forEach((str,i) => {result += Number(str) * based[i]});
-
-    return result;
+    let result = convert(time, value);
+    return _.reduce(
+        result, 
+        (memo, num, index) => memo + (Number(num) * alpha.timer_based[index]), 
+        0
+    );
 }
