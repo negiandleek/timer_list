457eae52bbaa4700b632c605f7c6086f7afa4df1
youya66 <youya66_official@gmail.com>
2018-06-19 15:46:59 +0900

    add pin_right_function to use input form

--- src/scripts/components/TimerForm.js
+++ src/scripts/components/TimerForm.js
@@ -3,63 +3,92 @@ import PropTypes from "prop-types";
 import ticktack from "../modules/index";
 import * as utils from "../utils";
 
-const TimerForm = (props) => {
-    let for_display = ticktack.display(props.form.count);
-
-    function handle_change(e){
-        const undisp = ticktack.undisplay(e.target.value);
-        let result = ticktack.shift_time_to_input(props.form.count, undisp);
-        if(!/^[0-9]+$/g.test(result)){
-            result = props.form.count;
-        };
-        props.change_input(result);
+function pin_right(_prev, _next){
+    let origin = _next;
+    let next = _next.slice(1,4);
+    let prev = _prev.slice(1);
+    let diff = "";
+    let index = 0;
+    if(next === prev)return origin;
+    
+    for(;index < next.length; index+=1){
+        if(next[index] !== prev[index]){
+            diff = next[index];
+            break;
+        }
     }
+    let pro = origin.split("");
+    pro.splice(index + 1, 1);
+    pro.push(diff);
+    return pro.join("");;
+}
 
-    function handle_click(alarmFlag){
-        if(!Number(props.form.count)){
+export default class TimerForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.my_ref = React.createRef();
+    }
+    handle_change(e){
+        let value = e.target.value;
+        let undisp = ticktack.undisplay(value);
+        if(undisp.length === 5){
+            undisp = pin_right(this.props.form.count, undisp);
+        }
+        let result = ticktack.shift_time_to_input(this.props.form.count, undisp);
+        if(!/^[0-9]+$/g.test(result)){
+            result = this.props.form.count;
+        };
+        this.props.change_input(result);
+    }
+    handle_click(alarmFlag){
+        if(!Number(this.props.form.count)){
             return void 0;
         }
         
-        let date = ticktack.generate_in_date_time(props.form.count, alarmFlag);
+        let date = ticktack.generate_in_date_time(this.props.form.count, alarmFlag);
         let diff = utils.get_diff_date_and_now(date)
         let count = utils.get_count(diff, alarmFlag)
         
-        props.add_timer(
+        this.props.add_timer(
             count,
             date,
             alarmFlag? true: false
         )
-        props.init_input();
+        this.props.init_input();
     }
-    return (
-        <div className="timer_input">
-            <form 
-                className="timer_form"
-                action="javascript:void(0)"
-            >
-                <input 
-                    className="timer_form__text"
-                    type="text"
-                    value={for_display}
-                    onChange={(e) => handle_change(e)}
-                />
-                <div className="form_btns">
+    render(){
+        let for_display = ticktack.display(this.props.form.count);
+        return (
+            <div className="timer_input">
+                <form 
+                    className="timer_form"
+                    action="javascript:void(0)"
+                >
                     <input 
-                        className="form_btns__btn"
-                        type="button" 
-                        value="count down"
-                        onClick={handle_click.bind(this, false)}
+                        className="timer_form__text"
+                        type="text"
+                        value={for_display}
+                        onChange={(e) => this.handle_change(e)}
+                        ref={this.my_ref}
                     />
-                    <input 
-                        className="form_btns__btn"
-                        type="button"
-                        value="alarm"
-                        onClick={handle_click.bind(this, true)}
-                    />
-                </div>
-            </form>
-        </div>
-    )
+                    <div className="form_btns">
+                        <input 
+                            className="form_btns__btn"
+                            type="button" 
+                            value="count down"
+                            onClick={this.handle_click.bind(this, false)}
+                        />
+                        <input 
+                            className="form_btns__btn"
+                            type="button"
+                            value="alarm"
+                            onClick={this.handle_click.bind(this, true)}
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
 
 TimerForm.propTypes = {
@@ -68,5 +97,3 @@ TimerForm.propTypes = {
         type: PropTypes.number.isRequired
     })
 }
-
-export default TimerForm;
