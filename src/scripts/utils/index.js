45281361413494dc3d25972a543a39c02c1c0d4a
youya66 <youya66_official@gmail.com>
2018-06-19 18:54:09 +0900

    fix chime

--- src/scripts/utils/index.js
+++ src/scripts/utils/index.js
@@ -23,21 +23,28 @@ export function get_remaining_time(date, isAlarm){
 export function get_diff_date_and_now(target){
     return target.getTime() - new Date().getTime();
 };
-
+// TODO:
 export let chime = (function(){
     let urls = ["./chime.mp3", "./chime.ogg", "./chime.wav"];
     let mimes = ["audio/mp3", "audio/ogg", "audio/wav"];
     let audios = _.map(urls, (url)=> new Audio(url));
-    let ready = _.filter(audios, (audio, index) => "probably" === audio.canPlayType(mimes[index]))
-
-    //todo: refactoring;
-    let play = function (audio){
-        audio.state = 0;
+    let ready = _.filter(audios, (audio, index) => {
+        return "" !== audio.canPlayType(mimes[index])
+    });
+    let audio = "";
+    
+    if(ready.length !== 0){
+        audio = ready[0];
+        audio.status = 0;
         audio.addEventListener("loadeddata", ()=>{
             audio.state = 1;
         });
+    }
 
+    let play = function (audio){
         return function count_play_audio(count = 1, volume = 0.5){
+            if(!audio)return;
+
             if(audio.state === 0){
                 audio.addEventListener("loadeddata", ()=>{
                     audio.state = 1
@@ -45,6 +52,7 @@ export let chime = (function(){
                 });
                 return void 0;
             }
+
             let iterations = count - 1;
             audio.volume = volume;
             audio.state = 2;
@@ -60,14 +68,19 @@ export let chime = (function(){
         }
     }
     
+    let stop = function(audio){
+        return function(){
+            if(!audio)return;
+            audio.pause()
+            audio.currentTime = 0;
+            audio.state = 3;
+        }
+    }
+    
     return {
-        audio: ready[0],
-        play: play(ready[0]),
-        stop: ()=>{
-            ready[0].pause()
-            ready[0].currentTime = 0;
-            ready[0].state = 3;
-        },
+        audio: audio,
+        play: play(audio),
+        stop: stop(audio),
     };
 })();
 
