0528d44c47d40976b6d50b07b4be427f4ad1965b
youya66 <youya66_official@gmail.com>
2018-06-21 10:41:41 +0900

    add compose in generate_timer function

--- src/modules/utils/generateTimer.js
+++ src/modules/utils/generateTimer.js
@@ -1,13 +1,17 @@
+import _ from "underscore";
 import str_to_milli from "../lib/convertStrToMilli";
 import is_str from "../lib/isTimeOfString";
 import milli_to_time from "../lib/convertMilliToTime";
 import time_to_str from "../lib/concatenateTimeToStr";
 import pad_zero from "../lib/padZero";
 
+// index is from (hours -> 0, minutes -> 1, seconds -> 2, minutes -> 3)
 export default function generate_timer(clock, alarm=false, index=0){
-    is_str(clock);
-    let a = str_to_milli(clock, alarm, index);
-    let b = milli_to_time(a);
-    let c = time_to_str(b,pad_zero);
-    return c;
+    let composed = _.compose(
+        (time)=>time_to_str(time, pad_zero),
+        (ms)=>milli_to_time(ms),
+        ()=>str_to_milli(clock, alarm, index),
+        ()=>is_str(clock)
+    )
+    return composed();
 }
