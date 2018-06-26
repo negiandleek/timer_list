79bd5f8e886545d16ec9421ea26dcad8e2277edb
youya66 <youya66_official@gmail.com>
2018-06-26 14:44:24 +0900

    accept clock and time

--- src/modules/utils/calcBaseTime.js
+++ src/modules/utils/calcBaseTime.js
@@ -1,36 +1,49 @@
 import _ from "underscore";
 
+import clock_to_milli from "../lib/clockToMilli";
+import pad_time from "../lib/padTime";
 import pad_zero from "../lib/padZero";
+import milli_to_time from "../lib/milliToTime";
+import slice_clock from "../lib/sliceClock";
 import time_to_clock from "../lib/timeToClock";
 
-import clock_to_milli from "../lib/clockToMilli";
-import milli_to_time from "../lib/milliToTime";
-import time_to_milli from "../lib/timeToMilli";
-import slice_clock from "../lib/sliceClock";
+import is_time from "../lib/isTime";
+import is_clock from "../lib/isClock";
+import is_numeric from "../lib/isNumeric";
 
-export default function calc_base_time(clock, millis = "1000", start=0, end=1){
-    let result = _.compose(
-        (time)=>time_to_clock(time),
-        (time)=>{
-            return _.mapObject(time, (ms)=> pad_zero(ms, 2))
-        },
-        (ms)=>milli_to_time(parseInt(ms,10) + parseInt(millis,10)),
-        (time)=>time_to_milli(time),
-        (ms)=>milli_to_time(ms),
-        (clock)=>clock_to_milli(clock, false, start)
-    )(clock);
+export default function calc_base_time(value, sub = {seconds: 1}, start=0, end=1){
+    let subject;
+    let target;
+    let obj_flag = false;
+
+    if(_.isObject(value)){
+        is_time(value);
+        subject = time_to_clock(pad_time(value), pad_zero);
+        obj_flag = true;
+    }else{
+        is_clock(value);
+        subject = value;
+    }
+    subject = clock_to_milli(subject)
+
+    if(_.isObject(sub)){
+        is_time(sub);
+        target = time_to_clock(pad_time(sub), pad_zero);
+        target = clock_to_milli(target);
+    }else{
+        is_numeric(sub);
+        target = sub;
+    }
+
+    let result = parseInt(subject,10) + parseInt(target, 10)
 
     if(result < 0){
         return 0;
     }
-
-    return _.compose(
-        (clock)=>slice_clock(clock, start, end),
-        (time)=>time_to_clock(time),
-        (time)=>{
-            return _.mapObject(time, (ms, key)=> pad_zero(ms, 2));
-        },
-        (ms)=>milli_to_time(ms),
-        (clock)=>clock_to_milli(clock)
-    )(result);
+    let time = milli_to_time(result);
+    if(obj_flag){
+        return time
+    }else{
+        return slice_clock(time_to_clock(time, pad_zero), start, end)
+    }
 }
