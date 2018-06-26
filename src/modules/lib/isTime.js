045930b7e1dd66f21e1248d4db0d051f6f8e8c3f
youya66 <youya66_official@gmail.com>
2018-06-26 16:45:31 +0900

    modify to check properties

--- src/modules/lib/isTime.js
+++ src/modules/lib/isTime.js
@@ -1,5 +1,7 @@
-import alpha from "./alpha";
 import _ from "underscore";
+
+import alpha from "./alpha";
+import is_numeric from "./isNumeric";
 
 export default function is_time(time){
     if(time == null || time == ""){
@@ -8,13 +10,24 @@ export default function is_time(time){
     if(!_.isObject(time) || (_.isObject(time) && _.isArray(time))){
         throw new TypeError("arguments is time object");
     }
-    let keys = _.isArray(time)? time: _.keys(time);
-    _.each(keys, (key)=>{
+
+    let result = _.reduce(time, (memo, value, key)=>{
         if(alpha.time_units.indexOf(key) === -1 && alpha.time_head_units.indexOf(key) === -1){ 
-            console.error("object key must be hours, minutes, seconds, millis or h, m, s, ms");
+            console.error("Waring: object key must be hours, minutes, seconds, millis or h, m, s, ms");
+        }else{
+            memo[key] = value;
+        }
+        return memo;
+    },{});
+
+    _.each(result, (value)=>{
+        try{
+            is_numeric(value);
+        }catch(e){
+            let message = e.message.slice(4);
+            throw new Error("property of time unit key " + message);
         }
     });
+
     return true;
 }
-
-is_time({hours: 0});
