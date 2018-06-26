8469d4a41cc349fdee70b4449fec6f14c7d7118d
youya66 <youya66_official@gmail.com>
2018-06-26 16:21:07 +0900

    accept clock and time

--- src/modules/utils/calcBaseTen.js
+++ src/modules/utils/calcBaseTen.js
@@ -1,20 +1,49 @@
 import _ from "underscore";
 
+import alpha from "../lib/alpha";
 import calculation from  "../lib/putTimeBaseTen";
-import pad_zero from "../lib/padZero";
-import time_to_clock from "../lib/timeToClock";
+import clock_to_time from "../lib/clockToTime";
 import milli_to_time from "../lib/milliToTime";
+import pad_zero from "../lib/padZero";
+import pad_time from "../lib/padTime";
+import time_to_clock from "../lib/timeToClock";
 import slice_clock from "../lib/sliceClock";
-import to_time from "../lib/clockToTime";
 
-export default function calc_base_ten(clock, ms, start=0, end=1){
-    let sign = Math.sign(parseInt(ms,10));
-    let origin = to_time(clock, start, end);
-    let calc = _.compose(
-        (clock)=>to_time(clock, start, end),
-        (clock)=>slice_clock(clock, start, end),
-        (time)=>time_to_clock(time, pad_zero),
-        (ms)=>milli_to_time(Math.abs(ms)),
-    )(ms);
-    return calculation(origin, calc, sign);
+import is_clock from "../lib/isClock";
+import is_time from "../lib/isTime";
+import is_numeric from "../lib/isNumeric";
+
+export default function calc_base_ten(value, sub={seconds: 1}, start=0, end=1){
+    let subject;
+    let target;
+    let clock_flag = false;
+
+    if(_.isObject(value)){
+        is_time(value);
+        subject = pad_time(value);
+    }else{
+        is_clock(value);
+        subject = clock_to_time(value, start, end);
+        clock_flag = true;
+    }
+    
+    if(_.isObject(sub)){
+        is_time(sub);
+        target = pad_time(sub);
+    }else{
+        is_numeric(sub);
+        target = milli_to_time(sub);
+    }
+    
+    let result = calculation(subject, target);
+    
+    if(result < 0){
+        result = alpha.clock_init;
+    }
+
+    if(clock_flag){
+        return slice_clock(time_to_clock(result, pad_zero), start, end);
+    }else{
+        return result;
+    }
 }
