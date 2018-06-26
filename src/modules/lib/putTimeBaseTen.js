f5e0ee4edbb47c9a85fec0b70c23646bc30b4ad3
youya66 <youya66_official@gmail.com>
2018-06-26 12:36:47 +0900

    fix subtraction

--- src/modules/lib/putTimeBaseTen.js
+++ src/modules/lib/putTimeBaseTen.js
@@ -1,22 +1,28 @@
 import alpha from "./alpha";
 
-export default function calculation(subject, target, sign = 1){
+export default function calculation(subject, target){
     let result = Object.assign({}, subject);
-    let base = sign !== -1? [100,100,100,1000]: [99,60,60,1000];
-
+    let base = {
+        "plus": [100,100,100,1000],
+        "minus": [99,60,60,1000]
+    }
+    
     for(let i = 3; i >= 0; i -= 1){
-        let diff = result[alpha.time_units[i]] + sign * target[alpha.time_units[i]];
+        let diff = result[alpha.time_units[i]] + target[alpha.time_units[i]];
+
         if(diff < 0 && i === 0){
             return Object.assign({}, alpha.time_init);
         }
-        if(diff < 0 && sign === -1){
-            diff = base[i] + diff;
+
+        if(diff < 0 && Math.sign(diff) === -1){
+            diff = base["minus"][i] + diff;
             result[alpha.time_units[(i - 1)]] -= 1;
         }
-        if(diff >= base[i] && sign === 1){
-            diff = diff - base[i];
+        if(diff >= base["plus"][i] && Math.sign(diff) === 1){
+            diff = diff - base["plus"][i];
             result[alpha.time_units[(i - 1)]] += 1;
         }
+
         result[alpha.time_units[i]] = diff;
     }
 
